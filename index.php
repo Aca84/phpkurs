c473e573ddb1c9e89e9002e4eeb64565be698dc1
Aleksandar <41162669+Aca84@users.noreply.github.com>
2019-07-04 21:03:44 +0200

    Master izmena

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 <<<<<<< HEAD
 	echo 'Ovo je tek pocetak,dvnb fdhjb ';
-?>
 =======
 	echo 'Ovo je tek pocetak';
 
@@ -10,4 +9,4 @@
 
 	echo 'brbrbbr';
 ?>
->>>>>>> 3b3e176f2d15f2090afa88dbc438050d571e15c1
+
