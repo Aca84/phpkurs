9c81af2f2d90d01e7a7cb8bfce8c07997554d76a
Aleksandar <41162669+Aca84@users.noreply.github.com>
2019-07-04 20:56:56 +0200

    test
    aaa

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 
-<<<<<<< HEAD
+
 	echo 'Ovo je tek pocetak,dvnb fdhjb ';
-?>
-=======
+
+
 	echo 'Ovo je tek pocetak';
 
 	echo 'Tatattatiraa';
 
+
 	echo 'brbrbbr';
+	
+	echo 'test grana';
 ?>
->>>>>>> 3b3e176f2d15f2090afa88dbc438050d571e15c1
+
